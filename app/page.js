9f156fcef296443239534e9e410f38d521621272
Andy <relic606@yahoo.com>
2023-08-10 16:19:38 -0600

    initial test structure added

--- app/page.js
+++ app/page.js
@@ -1,5 +1,64 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import VIDEOCLIPS from "../public/videodetails.js";
 
 export default function Home() {
-  return <main>Hello World</main>;
+  const [videoArr, setVideoArr] = useState(VIDEOCLIPS);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  function incrSelectedIndex() {
+    if (selectedIndex !== videoArr.length - 1) {
+      const newIndex = selectedIndex + 1;
+      setSelectedIndex(newIndex);
+    }
+  }
+  function decrSelectedIndex() {
+    if (selectedIndex !== 0) {
+      const newIndex = selectedIndex - 1;
+      setSelectedIndex(newIndex);
+    }
+  }
+
+  return (
+    <main className="flex justify-center">
+      <button onClick={decrSelectedIndex} className="border">
+        Previous Video
+      </button>
+
+      {selectedIndex > 0 ? (
+        <div>
+          <p>
+            {videoArr[selectedIndex - 1].hero} - {selectedIndex - 1}
+          </p>
+          <video src={videoArr[selectedIndex - 1].URL} className="w-60"></video>
+        </div>
+      ) : (
+        <div className="w-60"></div>
+      )}
+      <div>
+        <p>
+          {videoArr[selectedIndex].hero} - {selectedIndex}
+        </p>
+        <video
+          src={videoArr[selectedIndex].URL}
+          controls
+          className="w-96"
+        ></video>
+      </div>
+      {selectedIndex !== videoArr.length - 1 ? (
+        <div>
+          <p>
+            {videoArr[selectedIndex + 1].hero} - {selectedIndex + 1}
+          </p>
+          <video src={videoArr[selectedIndex + 1].URL} className="w-60"></video>
+        </div>
+      ) : (
+        <div className="w-60"></div>
+      )}
+      <button onClick={incrSelectedIndex} className="border">
+        Next Video
+      </button>
+    </main>
+  );
 }
---
+++ public/videodetails.js
@@ -0,0 +1,19 @@
+const VIDEOCLIPS = [
+  {
+    hero: "Ana",
+    type: "highlight",
+    URL: "https://drive.google.com/uc?id=1sc8lzNoeoENnAXujAavYgWlXv4rfgL2a",
+  },
+  {
+    hero: "Ana",
+    type: "highlight",
+    URL: "https://drive.google.com/uc?id=1sc8lzNoeoENnAXujAavYgWlXv4rfgL2a",
+  },
+  {
+    hero: "Ana",
+    type: "highlight",
+    URL: "https://drive.google.com/uc?id=1sc8lzNoeoENnAXujAavYgWlXv4rfgL2a",
+  },
+];
+
+export default VIDEOCLIPS;
