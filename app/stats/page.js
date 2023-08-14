676e6e5bdf4b083d4d5aadb0230c0211c2f18182
Andy <relic606@yahoo.com>
2023-08-14 16:22:20 -0600

    added profile search with error handling

--- app/stats/page.js
+++ app/stats/page.js
@@ -4,49 +4,100 @@ import { useEffect, useState } from "react";
 
 export default function Stats() {
   const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
   const [profile, setProfile] = useState("Relic-1203");
 
-  const [inputField, setInputField] = useState();
+  const [inputFieldBt, setInputFieldBt] = useState("");
+  const [inputFieldId, setInputFieldId] = useState("");
 
-  const inputsHandler = (e) => {
-    setInputField({ [e.target.name]: e.target.value });
+  const newProfile = () => {
+    if (inputFieldBt.length === 0 || inputFieldId.length === 0) {
+      alert("Please provide a Battletag and ID");
+    } else {
+      const formattedBT =
+        inputFieldBt.charAt(0).toUpperCase() +
+        inputFieldBt.slice(1).toLowerCase();
+      setProfile(formattedBT + "-" + inputFieldId);
+      setData(null);
+    }
+  };
+
+  const inputHandlerBt = (e) => {
+    setInputFieldBt(e.target.value);
+  };
+  const inputHandlerId = (e) => {
+    setInputFieldId(e.target.value);
   };
 
   const submitButton = () => {
-    alert(inputField.first_name);
+    newProfile();
   };
 
   useEffect(() => {
     fetch(`https://ow-api.com/v1/stats/pc/us/${profile}/complete/`)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
         setData(data);
-        setLoading(false);
       });
-  }, []);
+  }, [profile]);
 
-  if (isLoading) {
+  if (!data) {
     return <p>Loading...</p>;
-  } else if (!data) {
-    return <p>No profile data</p>;
   } else {
-    return (
-      <div>
-        {/* <input
-          type="text"
-          name="first_name"
-          onChange={inputsHandler}
-          placeholder="Battletag"
-          value={inputField.first_name}
-        />
+    if (data.error === "Player not found") {
+      return (
+        <div>
+          <input
+            type="text"
+            onChange={inputHandlerBt}
+            placeholder="Battletag"
+            className="border m-4"
+          />
+          <input
+            type="text"
+            onChange={inputHandlerId}
+            placeholder="ID"
+            className="border"
+          />
+          <button onClick={submitButton} className="mx-4 border">
+            Search Profile
+          </button>
+          <p>Player Not Found</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input
+            type="text"
+            onChange={inputHandlerBt}
+            placeholder="Battletag"
+            className="border m-4"
+          />
+          <input
+            type="text"
+            onChange={inputHandlerId}
+            placeholder="ID"
+            className="border"
+          />
 
-        <button onClick={submitButton}>Submit Now</button> */}
-        <p>{data.name}</p>
-        <pre>
-          <code>{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      </div>
-    );
+          <button onClick={submitButton} className="mx-4 border">
+            Search for Profile
+          </button>
+          <div className="flex">
+            <img src={data.icon} className="mx-8"></img>
+            <div>
+              <p className="text-2xl font-bold">{data.name}</p>
+              <p>Games Played - {data.gamesPlayed}</p>
+              <p>Won - {data.gamesWon}</p>
+              <p>Lost - {data.gamesLost}</p>
+            </div>
+          </div>
+          <pre>
+            <code>{JSON.stringify(data, null, 2)}</code>
+          </pre>
+        </div>
+      );
+    }
   }
 }
