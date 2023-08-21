fd161f6638bff13c2590f7fcb72f73eb4805e80f
Andy Anderson <relic606@yahoo.com>
2023-08-21 09:11:52 -0600

    added hero checkbox on highlight page

--- app/page.js
+++ app/page.js
@@ -5,121 +5,159 @@ import VIDEOCLIPS from "../public/videodetails.js";
 import Link from "next/link";
 
 export default function Home() {
-  const [videoArr, setVideoArr] = useState(VIDEOCLIPS);
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [selectedHero, setSelectedHero] = useState("default");
+	const [selectedIndex, setSelectedIndex] = useState(0);
+	const [selectedHero, setSelectedHero] = useState("default");
 
-  const initHeroArr = [];
-  videoArr.forEach(function (highlight, index) {
-    if (!initHeroArr.includes(highlight.hero)) {
-      initHeroArr.push(highlight.hero);
-    }
-  });
+	const [videoArr, setVideoArr] = useState(VIDEOCLIPS);
 
-  const [heroArr, setHeroArr] = useState(initHeroArr);
+	const initHeroArr = [];
+	videoArr.forEach(function (highlight, index) {
+		if (!initHeroArr.includes(highlight.hero)) {
+			initHeroArr.push(highlight.hero);
+		}
+	});
 
-  function filterHero(e) {
-    setVideoArr(VIDEOCLIPS);
-    const newSelectedHero = e.target.value;
-    setSelectedHero(newSelectedHero);
-    setSelectedIndex(0);
-  }
+	const [heroArr, setHeroArr] = useState(initHeroArr);
 
-  function incrSelectedIndex() {
-    if (selectedIndex !== videoArr.length - 1) {
-      const newIndex = selectedIndex + 1;
-      setSelectedIndex(newIndex);
-    }
-  }
-  function decrSelectedIndex() {
-    if (selectedIndex !== 0) {
-      const newIndex = selectedIndex - 1;
-      setSelectedIndex(newIndex);
-    }
-  }
+	function filterHero(e) {
+		setVideoArr(VIDEOCLIPS);
+		const newSelectedHero = e.target.value;
+		setSelectedHero(newSelectedHero);
+		setSelectedIndex(0);
+	}
 
-  useEffect(() => {
-    if (selectedHero !== "default") {
-      const newVideoArr = videoArr;
-      const filteredArr = newVideoArr.filter((highlight) => {
-        return highlight.hero === selectedHero;
-      });
-      setVideoArr(filteredArr);
-    }
-  }, [selectedHero]);
+	function incrSelectedIndex() {
+		if (selectedIndex !== videoArr.length - 1) {
+			const newIndex = selectedIndex + 1;
+			setSelectedIndex(newIndex);
+		}
+	}
+	function decrSelectedIndex() {
+		if (selectedIndex !== 0) {
+			const newIndex = selectedIndex - 1;
+			setSelectedIndex(newIndex);
+		}
+	}
 
-  return (
-    <main className="flex justify-center h-screen">
-      <div className="bg-[url('/ow-bg4.png')] bg-cover background-container"></div>
-      <div className="h-fit w-fit absolute left-0 bg-white border-black border p-1">
-        <label for="hero-select">Choose a Hero:</label>
-        <select
-          name="heros"
-          id="hero-select"
-          onChange={filterHero}
-          value={selectedHero}
-        >
-          <option value="default">- All Heroes -</option>
-          {heroArr.map((hero) => {
-            return (
-              <option key={hero} value={hero}>
-                {hero}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div className="flex my-auto p-8 z-10">
-        {selectedIndex > 0 ? (
-          <div className="my-auto bg-black rounded-md text-white p-4">
-            <iframe
-              src={`${videoArr[selectedIndex - 1].URL}?controls=0`}
-              frameborder="0"
-              allow="autoplay; encrypted-media"
-              title="video"
-              className="side-video"
-            />
-            <button onClick={decrSelectedIndex} className="border p-2">
-              Previous Video
-            </button>
-          </div>
-        ) : (
-          <div className="side-video"></div>
-        )}
+	const [checkedImages, setCheckedImages] = useState([]);
 
-        <div className="bg-black text-white mx-4 p-4 pb-8 rounded-md">
-          <iframe
-            src={videoArr[selectedIndex].URL}
-            frameborder="0"
-            modestbranding
-            allow="autoplay; encrypted-media"
-            allowFullScreen="allowfullscreen"
-            mozallowfullscreen="mozallowfullscreen"
-            msallowfullscreen="msallowfullscreen"
-            oallowfullscreen="oallowfullscreen"
-            webkitallowfullscreen="webkitallowfullscreen"
-            title="video"
-            className="center-video"
-          />
-        </div>
-        {selectedIndex !== videoArr.length - 1 ? (
-          <div className="my-auto bg-black rounded-md text-white p-4">
-            <iframe
-              src={`${videoArr[selectedIndex + 1].URL}?controls=0`}
-              frameborder="0"
-              modestbranding
-              allow="autoplay; encrypted-media"
-              title="video"
-              className="side-video"
-            />
-            <button onClick={incrSelectedIndex} className="border p-2">
-              Next Video
-            </button>
-          </div>
-        ) : (
-          <div className="side-video"></div>
-        )}
-      </div>
-    </main>
-  );
+	const toggleImage = (imageSrc) => {
+		if (checkedImages.includes(imageSrc)) {
+			setCheckedImages(checkedImages.filter((src) => src !== imageSrc));
+		} else {
+			setCheckedImages([...checkedImages, imageSrc]);
+		}
+	};
+
+	useEffect(() => {
+		if (selectedHero !== "default") {
+			const newVideoArr = videoArr;
+			const filteredArr = newVideoArr.filter((highlight) => {
+				return highlight.hero === selectedHero;
+			});
+			setVideoArr(filteredArr);
+		}
+	}, [selectedHero]);
+
+	useEffect(() => {
+		const newVideoArr = VIDEOCLIPS;
+
+		console.log(videoArr);
+		console.log(checkedImages);
+		setSelectedIndex(0);
+	}, [checkedImages]);
+
+	return (
+		<main className="flex flex-col justify-center h-screen">
+			<div className="bg-[url('/ow-bg4.png')] bg-cover background-container"></div>
+			{/* <div className="h-fit w-fit absolute left-0 bg-white border-black border p-1">
+				<label for="hero-select">Choose a Hero:</label>
+				<select
+					name="heros"
+					id="hero-select"
+					onChange={filterHero}
+					value={selectedHero}
+				>
+					<option value="default">- All Heroes -</option>
+					{heroArr.map((hero) => {
+						return (
+							<option key={hero} value={hero}>
+								{hero}
+							</option>
+						);
+					})}
+				</select>
+			</div> */}
+			<div className="image-checkboxes flex justify-center">
+				<label className="checkbox-label">
+					<input type="checkbox" className="checkbox-input" />
+					<img
+						src="/icons/Icon-Ana.webp"
+						alt="ana"
+						className={checkedImages.includes("Ana") ? "checked h-24" : "h-24"}
+						onClick={() => toggleImage("Ana")}
+					/>
+				</label>
+				<label className="checkbox-label">
+					<input type="checkbox" className="checkbox-input" />
+					<img
+						src="/icons/Icon-Genji.webp"
+						alt="genji"
+						className={
+							checkedImages.includes("Genji") ? "checked h-24" : "h-24"
+						}
+						onClick={() => toggleImage("Genji")}
+					/>
+				</label>
+			</div>
+
+			<div className="flex my-auto p-8 z-10">
+				{selectedIndex > 0 ? (
+					<div className="my-auto bg-black rounded-md text-white p-4">
+						<img
+							src={videoArr[selectedIndex - 1].img}
+							className="side-video"
+							alt="video preview"
+						/>
+						<button onClick={decrSelectedIndex} className="border p-2">
+							Previous Video
+						</button>
+					</div>
+				) : (
+					<div className="side-video"></div>
+				)}
+
+				<div className="bg-black text-white mx-4 p-4 pb-8 rounded-md">
+					<iframe
+						src={videoArr[selectedIndex].URL}
+						frameborder="0"
+						modestbranding
+						allow="autoplay; encrypted-media"
+						allowFullScreen="allowfullscreen"
+						mozallowfullscreen="mozallowfullscreen"
+						msallowfullscreen="msallowfullscreen"
+						oallowfullscreen="oallowfullscreen"
+						webkitallowfullscreen="webkitallowfullscreen"
+						title="video"
+						className="center-video"
+					/>
+				</div>
+
+				{selectedIndex !== videoArr.length - 1 ? (
+					<div className="my-auto bg-black rounded-md text-white p-4">
+						<img
+							src={videoArr[selectedIndex + 1].img}
+							className="side-video"
+							alt="video preview"
+						/>
+						<button onClick={incrSelectedIndex} className="border p-2">
+							Next Video
+						</button>
+					</div>
+				) : (
+					<div className="side-video"></div>
+				)}
+			</div>
+		</main>
+	);
 }
--- public/videodetails.js
+++ public/videodetails.js
@@ -1,86 +1,102 @@
 const VIDEOCLIPS = [
-  {
-    hero: "Ana",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/HCmHagP1fiQ",
-  },
-  {
-    hero: "Ana",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/0jMhldPdnI4",
-  },
-  {
-    hero: "Doomfist",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/2N8uqM6ZxjE",
-  },
-  {
-    hero: "Genji",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/cZp6Uj7vqV8",
-  },
-  {
-    hero: "Genji",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/ZtkEP8CBKqo",
-  },
-  {
-    hero: "Genji",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/vC8NlDkmnco",
-  },
-  {
-    hero: "Genji",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/udUZCr-Zx7I",
-  },
-  {
-    hero: "Genji",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/vH5OVLpzOHg",
-  },
-  {
-    hero: "Genji",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/IEWvTpdKTdE",
-  },
-  {
-    hero: "Genji",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/rhTlVFlxHRs",
-  },
-  {
-    hero: "Genji",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/sFYxpQXwOIk",
-  },
-  {
-    hero: "Genji",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/JysMIJ02ntk",
-  },
+	{
+		hero: "Ana",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/HCmHagP1fiQ",
+		img: "http://img.youtube.com/vi_webp/HCmHagP1fiQ/0.webp"
+	},
+	{
+		hero: "Ana",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/0jMhldPdnI4",
+		img: "http://img.youtube.com/vi_webp/0jMhldPdnI4/0.webp"
+	},
+	{
+		hero: "Doomfist",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/2N8uqM6ZxjE",
+		img: "http://img.youtube.com/vi_webp/2N8uqM6ZxjE/0.webp"
+	},
+	{
+		hero: "Genji",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/cZp6Uj7vqV8",
+		img: "http://img.youtube.com/vi_webp/cZp6Uj7vqV8/0.webp"
+	},
+	{
+		hero: "Genji",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/ZtkEP8CBKqo",
+		img: "http://img.youtube.com/vi_webp/ZtkEP8CBKqo/0.webp"
+	},
+	{
+		hero: "Genji",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/vC8NlDkmnco",
+		img: "http://img.youtube.com/vi_webp/vC8NlDkmnco/0.webp"
+	},
+	{
+		hero: "Genji",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/udUZCr-Zx7I",
+		img: "http://img.youtube.com/vi_webp/udUZCr-Zx7I/0.webp"
+	},
+	{
+		hero: "Genji",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/vH5OVLpzOHg",
+		img: "http://img.youtube.com/vi_webp/vH5OVLpzOHg/0.webp"
+	},
+	{
+		hero: "Genji",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/IEWvTpdKTdE",
+		img: "http://img.youtube.com/vi_webp/IEWvTpdKTdE/0.webp"
+	},
+	{
+		hero: "Genji",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/rhTlVFlxHRs",
+		img: "http://img.youtube.com/vi_webp/rhTlVFlxHRs/0.webp"
+	},
+	{
+		hero: "Genji",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/sFYxpQXwOIk",
+		img: "http://img.youtube.com/vi_webp/sFYxpQXwOIk/0.webp"
+	},
+	{
+		hero: "Genji",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/JysMIJ02ntk",
+		img: "http://img.youtube.com/vi_webp/JysMIJ02ntk/0.webp"
+	},
 
-  {
-    hero: "Sojourn",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/ZXPaISVmpm8",
-  },
+	{
+		hero: "Sojourn",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/ZXPaISVmpm8",
+		img: "http://img.youtube.com/vi_webp/ZXPaISVmpm8/0.webp"
+	},
 
-  {
-    hero: "Widow",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/Zu4ZYo9_pQE",
-  },
-  {
-    hero: "Widow",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/E2Xxq9H1TtI",
-  },
-  {
-    hero: "Widow",
-    type: "highlight",
-    URL: "https://www.youtube.com/embed/3Xp059V98kA",
-  },
+	{
+		hero: "Widow",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/Zu4ZYo9_pQE",
+		img: "http://img.youtube.com/vi_webp/Zu4ZYo9_pQE/0.webp"
+	},
+	{
+		hero: "Widow",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/E2Xxq9H1TtI",
+		img: "http://img.youtube.com/vi_webp/E2Xxq9H1TtI/0.webp"
+	},
+	{
+		hero: "Widow",
+		type: "highlight",
+		URL: "https://www.youtube.com/embed/3Xp059V98kA",
+		img: "http://img.youtube.com/vi_webp/3Xp059V98kA/0.webp"
+	}
 ];
 
 export default VIDEOCLIPS;
