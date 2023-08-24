f9817002f2a4e6481a5a6229d11fafdbd1e2be18
Andy <relic606@yahoo.com>
2023-08-24 14:09:30 -0600

    added basepath config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/projects/overwatch",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
